b34ac041faf6389587f45227641dcbc587eb950e
podski-holobits <piotr@holobits.pl>
2020-05-05 22:42:11 +0200

    writing configs for webpack #6. All initial dependencies added. Stats.js are working

--- src/init.js
+++ src/init.js
@@ -1,1 +1,9 @@
 import './css/main.scss' //Loads scss into projects - init.js is the starting point for webpack
+import ready from 'domready';
+import App from './app.js';
+
+ready(() => {
+	window.app = new App();
+	window.app.initStats();
+	window.app.animateStats();
+});
